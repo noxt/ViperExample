714ce375e29c3e0464918fb2c07b32d4eda191e9
Dmitry Ivanenko <id.noxt@gmail.com>
2016-11-17 22:49:46 +0300

    Update .gitignore

--- Pods/Target Support Files/TableKit/TableKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TableKit : NSObject
-@end
-@implementation PodsDummy_TableKit
-@end
--- Pods/Target Support Files/TableKit/TableKit-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double TableKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char TableKitVersionString[];
-
